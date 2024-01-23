32313626f1ed4071257a41c6735ab41bdd775e72
Momodayoo <msyauqi94@gmail.com>
2024-01-23 20:57:46 +0800

    commit on mo

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+const User = require("../models/user");
+/**
+ *
+ * @returns {PromiseArray<User>}
+ */
+const getUsers = async () => {
+    const data = await User.findAll({});
+    return data;
+};
+/**
+ *
+ * @param {number} id - user id
+ * @returns {Promise<User>}
+ */
+const getUser = async (id) => {
+  const data = await User.findOne({ where: { id: id } });
+  return data;
+};
+/**
+ *
+ * @param {User} data - user data
+ * @returns {Promise<User>}
+ */
+const createUser = async (data) => {
+  const user = await User.create(data);
+  return user;
+};
+/**
+ *
+ * @param {number} id - user id
+ * @param {User} data - user data
+ * @returns {Promise<User>}
+ */
+const updateUser = async (id, data) => {
+  const user = await User.update(data, { where: { id: id } });
+  return user;
+};
+/**
+ *
+ * @param {number} id - user id
+ * @returns {Promise<User>}
+ */
+const deleteUser = async (id) => {
+  const user = await User.destroy({ where: { id: id } });
+  return user;
+};
+// exports functions for use in routes
+module.exports = {
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 
 const app = express();
+
+//Database
+const db = require("./db");
+//create table
+const models = require("./models");
+models.init();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
