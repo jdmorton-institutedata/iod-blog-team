04517f7e87b0f87fc2b6b488a495ef85cba0ccb0
John Morton <john.morton@institutedata.com>
2024-01-25 20:14:50 +0800

    Add middleware to pipeline

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const { handleInvalidJson, handleUnauthorized, handleNotFound, handleAllOtherErrors } = require("./errors/errorHandler");
+const morganMiddleware = require("./logging/morganMiddleware");
+const Logger = require("./logging/logger");
 
 // Database
 const db = require("./db");
@@ -9,12 +12,38 @@ models.init();
 
 const app = express();
 
+app.use(express.json());
+
+app.use(morganMiddleware);
+
+// Swagger
+if (process.env.NODE_ENV === 'development') {
+  const swaggerUi = require('swagger-ui-express');
+  const swaggerSpec = require('./swagger/swaggerSpec');
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec.default));
+}
+
+// // Routes
+app.use("/api/users", require("./routes/userRoutes"));
+// add post routes
+app.use("/api/posts", require("./routes/postRoutes"));
+// add comment routes
+app.use("/api/comments", require("./routes/commentRoutes"));
+// add like routes
+app.use("/api/likes", require("./routes/likeRoutes"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Add error handler middleware functions to the pipeline
+app.use(handleInvalidJson);
+app.use(handleUnauthorized);
+app.use(handleNotFound);
+app.use(handleAllOtherErrors);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  Logger.debug(`Example app listening on port ${port}!`);
 });
