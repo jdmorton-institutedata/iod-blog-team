7715fc033f5859510a2cbc120fabc303fe2fe900
Momodayoo <msyauqi94@gmail.com>
2024-01-25 18:29:27 +0800

    add controllers

---
+++ controllers/commentController.js
@@ -0,0 +1,84 @@
+// comments controller
+const Comment = require('../models/comment');
+/**
+ *
+ * @returns {Promise<Array<Comment>>}
+ */
+const getComments = async () => {
+    const data = await Comment.findAll({});
+    return data;
+};
+/**
+ *
+ * @param {number} id - comment id
+ * @returns {Promise<Comment>}
+ **/
+const getComment = async (id) => {
+    const data = await Comment.findOne({ where: { id: id } });
+    return data;
+};
+/**
+ *
+ * @param {number} id - comment id
+ * @returns {Promise<Comment>}
+ */
+const getCommentIncludeAll = async (id) => {
+    const data = await Comment.findOne({ where: { id: id }, include: { all: true } });
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Array<Comment>>}
+ * */
+const getCommentsByPost = async (id) => {
+    const data = await Comment.findAll({ where: { postId: id } });
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Array<Comment>>}
+ */
+const getCommentsByUser = async (id) => {
+    const data = await Comment.findAll({ where: { userId: id } });
+    return data;
+};
+/**
+ *
+ * @param {object} data
+ * @returns {Promise<Comment>}
+ */
+const createComment = async (data) => {
+    const comment = await Comment.create(data);
+    return comment;
+};
+/**
+ *
+ * @param {int} id
+ * @param {object} data
+ * @returns {Promise<Comment>}
+ */
+const updateComment = async (id, data) => {
+    const comment = await Comment.update(data, { where: { id: id } });
+    return comment;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Comment>}
+ */
+const deleteComment = async (id) => {
+    const comment = await Comment.destroy({ where: { id: id } });
+    return comment;
+};
+module.exports = {
+    getComments,
+    createComment,
+    updateComment,
+    deleteComment,
+    getComment,
+    getCommentIncludeAll,
+    getCommentsByPost,
+    getCommentsByUser,
+};
---
+++ controllers/likeController.js
@@ -0,0 +1,78 @@
+const Like = require('../models/like');
+/**
+ *
+ * @returns {Promise<Array<Like>>}
+ */
+const getLikes = async () => {
+    const data = await Like.findAll({});
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Like>}
+ */
+const getLike = async (id) => {
+    const data = await Like.findOne({ where: { id: id } });
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Like>}
+ */
+ const getLikeIncludeAll = async (id) => {
+    const data = await Like.findOne({ where: { id: id }, include: { all: true } });
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Array<Like>>}
+ * */
+const getLikesByPost = async (id) => {
+    const data = await Like.findAll({ where: { postId: id } });
+    return data;
+};
+/**
+ *
+ * @param {int} id
+ * @returns {Promise<Array<Like>>}
+ * */
+const getLikesByUser = async (id) => {
+    const data = await Like.findAll({ where: { userId: id } });
+    return data;
+};
+const createLike = async (data) => {
+    const like = await Like.create(data);
+    return like;
+};
+/**
+ *
+ * @param {int} id - like id
+ * @param {object} data - like data
+ * @returns {Promise<Like>}
+ * */
+const updateLike = async (id, data) => {
+    const like = await Like.update(data, { where: { id: id } });
+    return like;
+};
+/**
+ *
+ * @param {int} id - like id
+ * @returns {Promise<Like>}
+ * */
+const deleteLike = async (id) => {
+    const like = await Like.destroy({ where: { id: id } });
+    return like;
+};
+module.exports = {
+    getLikes,
+    createLike,
+    updateLike,
+    deleteLike,
+    getLike,
+    getLikeIncludeAll,
+    getLikesByPost,
+    getLikesByUser,
+};
