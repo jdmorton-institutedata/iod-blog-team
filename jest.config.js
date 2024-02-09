749f95bd85a11dcfe09f9de871fa1ed660487c1c
John Morton <john.morton@institutedata.com>
2024-02-09 12:11:22 +0800

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    collectCoverage: true,
+    coverageThreshold: {
+      global: {
+        branches: 75,
+        functions: 90,
+        lines: 80,
+      },
+    },
+    coverageReporters: ["lcov", "text"],
+    coverageDirectory: "./coverage-report/",
+    testEnvironment: "node",
+    testPathIgnorePatterns: ["/node_modules/", "/__tests__/fixtures/", "/__tests__/integration/"],
+    testRegex: "(/__tests__/unit/.*|(\\.|/)(test|spec))\\.jsx?$",
+    testTimeout: 10000,
+    globals: {
+      globalVar: "a global variable",
+    },
+    setupFiles: ["<rootDir>/test/jest-unit-setup.js"],
+  };
+  
